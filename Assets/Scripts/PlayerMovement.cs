4d7ebd2b18b68f3732b2843f2851e12165a35dbb
hyobeen12 <aszx8683@naver.com>
2024-10-18 11:19:27 +0900

    [Add]PlayerMovement
    add playermovement , animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,9 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
 
         transform.position += (new Vector3(x, y)).normalized * Time.deltaTime * speed;
+
+        
     }
 }
