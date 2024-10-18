b15602a99899caf962da5cdb91367e7d98e11682
hyobeen12 <aszx8683@naver.com>
2024-10-18 17:43:25 +0900

    [Add]RetryButton script

---
+++ Assets/Scripts/RetryButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+    public class RetryButton : MonoBehaviour
+    {
+        public void OnClickRetryBtn()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
